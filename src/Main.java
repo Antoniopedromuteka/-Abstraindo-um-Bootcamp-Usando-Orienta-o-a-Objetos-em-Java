2068c0ddd5c74d4d1d59c710ea269b2d5f621fb7
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-02-19 22:56:10 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -28,6 +30,52 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+
+
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMuteka = new Dev();
+        devMuteka.setNome("Muteka");
+        devMuteka.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteudos Inscritos Muteka" + devMuteka.getConteudosInscritos());
+        devMuteka.progredir();
+        devMuteka.progredir();
+
+        System.out.println("XP:" + devMuteka.calcularTotalXp());
+
+        System.out.println("---------------");
+
+
+        System.out.println("Conteudos Inscritos Muteka" + devMuteka.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Muteka" + devMuteka.getConteudosConcluidos());
+
+
+        Dev devFabio = new Dev();
+        devFabio.setNome("Fábio");
+        devFabio.inscreverBootcamp(bootcamp);
+        devFabio.progredir();
+        devFabio.progredir();
+        devFabio.progredir();
+        System.out.println("Conteudos Inscritos Fábio" + devFabio.getConteudosInscritos());
+        System.out.println("XP:" + devFabio.calcularTotalXp());
+
+
+
+
+        System.out.println("---------------");
+
+        System.out.println("Conteudos Inscritos Fábio" + devFabio.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Fábio" + devFabio.getConteudosConcluidos());
+
+
+
 
 
 
--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,30 +1,19 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int cargaHoraria;
+public class Curso extends Conteudo{
 
+    private int cargaHoraria;
 
     public Curso(){
 
     }
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp(){
+        return XP_PADRAO * cargaHoraria;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -37,8 +26,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
